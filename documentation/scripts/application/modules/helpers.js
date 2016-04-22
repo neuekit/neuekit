e575b69b72021a63ef9589dd708b387d87e7fe5d
CreativeLittleChris <chris@creativelittledots.co.uk>
2016-04-22 22:25:51 +0100

    Fixed Documentation scripts folders

---
+++ documentation/scripts/application/modules/helpers.js
@@ -0,0 +1,83 @@
+/*--------------------------------------------------*\
+	#PROJECT | JS HELPERS
+	
+	This script contains all the helper/utility
+	functions which we can use across our other
+	JS files.
+	
+	To start our script we wrap all of our code in a
+	self-executing anonymous function. We then pass
+	in 3 arguments to setup jQuery & our namespace.
+	
+	1. PROJECT is our namespace.
+	2. $ is defined for jQuery.
+	3. Lastly we ensure undefined really is undefined.
+\*--------------------------------------------------*/
+
+(function(PROJECT, $, undefined) {
+    
+    /*  'use strict' enforces correct syntax.  */
+    
+    'use strict';
+    
+    
+    /*--------------------------------------------------*\
+    	#PROJECT HELPER METHOD
+    	
+    	This is a 'singleton' which isolates the code
+    	inside from the global namespace, providing
+    	a single point of access for functions.
+    \*--------------------------------------------------*/
+    
+    PROJECT.helper = (function() {
+        
+        function _helper() {
+
+            /*  Variablise 'this' to limit it to avoid scope conflicts  */
+            /* jshint validthis: true */
+            var _this = this;
+
+
+
+
+            
+            /*--------------------------------------------------*\
+            	#YOUR HELPER FUNCTIONS HERE
+            \*--------------------------------------------------*/
+            
+            
+            
+            
+                        
+            
+            /*  Allow "chaining" of methods together  */
+
+            this.init = function() {
+                
+                
+                
+                
+                /*  'this' refers to PROJECT.helper  */
+                
+                return this;
+            };
+            
+            
+            /*  This refers to PROJECT.helper.init()  */
+            
+            return this.init();
+        }
+        
+        
+        /*  creating a new object of helper rather than a funtion */
+        
+        return new _helper();
+        
+    }());
+
+
+/* Lastly this checks if the namespace already exists & if not will assign it */
+
+}(window.PROJECT = window.PROJECT || {}, jQuery));
+
+
---
+++ documentation/scripts/application/settings.js
@@ -0,0 +1,31 @@
+/*--------------------------------------------------*\
+	#PROJECT | JS SETTINGS
+	
+	This script contains all the reusable data such
+	as urls, error messages, html created by JS &
+	repeated values & values that may change later.
+	
+	To start our script we wrap all of our code in a
+	self-executing anonymous function. We then pass
+	in 3 arguments to setup jQuery & our namespace.
+	
+	1. PROJECT is our namespace.
+	2. $ is defined for jQuery.
+	3. Lastly we ensure undefined really is undefined.
+\*--------------------------------------------------*/
+
+(function(PROJECT, $, undefined) {
+    
+    /* This is our global configuration. It will work across your js files */
+    
+    PROJECT.settings = {
+        debug: true,
+        speed: 250
+    };
+    
+    
+/* Lastly this checks if the namespace already exists & if not will assign it */
+
+}(window.PROJECT = window.PROJECT || {}, jQuery));
+
+
