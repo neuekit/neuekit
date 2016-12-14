a2899f999a1e73b72da5e1bc3cbdd3aa5dc56e94
Matt Pilott <matt@creativelittledots.co.uk>
2016-12-14 13:06:15 +0000

    Now using uglify harmony
    Signed-off-by: Matt Pilott <matt@creativelittledots.co.uk>

--- rollup.config.js
+++ rollup.config.js
@@ -4,31 +4,31 @@ import uglify from 'rollup-plugin-uglify';
 import { minify } from 'uglify-js';
 
 export default {
-    entry: './app/scripts/framework/framework.js',
-    dest: './app/scripts/framework/framework.min.js',
-    format: 'iife',
-    moduleName: 'UIKit',
-    moduleContext: {
+    entry : './app/scripts/framework/framework.js',
+    dest : './app/scripts/framework/framework.min.js',
+    format : 'iife',
+    moduleName : 'UIKit',
+    moduleContext : {
         './app/scripts/framework/parties/promise.js' : 'window',
         './app/scripts/framework/parties/fetch.js' : 'window'
     },
     plugins: [
         babel({
-            exclude: ['./node_modules/**', './app/scripts/framework/parties/**'],
-            "presets": [[
+            exclude : ['./node_modules/**', './app/scripts/framework/parties/**'],
+            presets : [[
                 "env", {
-                    "targets": {
-                        "browsers": ["> 1%", "last 2 versions", "Firefox ESR", "not ie_mob <= 12"]
+                    "targets" : {
+                        "browsers" : ["> 1%", "last 2 versions", "Firefox ESR", "not ie_mob <= 12"]
                     },
-                    "modules": false
+                    "modules" : false
                 }
             ]],
-            "plugins": [
+            plugins : [
                 "external-helpers"
             ]
         }),
         uglify({
-            screwIE8: true
+            screwIE8 : true
         }, minify)
     ]
 };
