dba28ba7e7b01e63b8bb2623360ca7dfb0dda253
Matt Pilott <matt@creativelittledots.co.uk>
2019-02-19 18:33:36 +0000

    alpha 7 with margin removed from justify utils

--- src/scripts/neuekit.js
+++ src/scripts/neuekit.js
@@ -1,7 +1,7 @@
 /*!---------------------------------------------------------------------------*\
     Neue Kit
 
-    - Version:      1.0.0.alpha.6
+    - Version:      1.0.0.alpha.7
     - Author:       Creative Little Dots
     - Site:         http://creativelittle.uk/neuekit
     - Copyright:    2019, Creative Little Dots
