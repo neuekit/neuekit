07b040420c787eb961b2f55291a515e83243d427
Matt Pilott <matt@creativelittledots.co.uk>
2019-02-22 11:11:19 +0000

    polyfill order

--- scripts/polyfills/polyfills.js
+++ scripts/polyfills/polyfills.js
@@ -1,15 +1,16 @@
 /*----------------------------------------------------------------------------*\
     #POLYFILLS
 
-    The features of tomorrow today! (Dont panic its only 6.56kb GZipped)
+    The features of tomorrow today! (Dont panic its only 6.58kb GZipped)
 \*----------------------------------------------------------------------------*/
 
-import 'custom-event-polyfill';
-import 'element-closest';
-import 'promise-polyfill';
-import 'smoothscroll-polyfill';
-import 'stickyfilljs';
-import 'svg4everybody';
-import 'whatwg-fetch';
+import 'custom-event-polyfill';     // IE11
+import 'element-closest';           // IE11
+import 'promise-polyfill';          // IE11
+import 'svg4everybody';             // IE11
+import 'whatwg-fetch';              // IE11
+
+import 'smoothscroll-polyfill';     // IE11, Edge, Safari, Android
+import 'stickyfilljs';              // IE11, Edge, Chrome, Opera, Android
 
 
