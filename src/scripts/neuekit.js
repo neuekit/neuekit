5d1eac53fb92375ef7a0a33188796e4b35468c5f
Matt Pilott <matt@creativelittledots.co.uk>
2019-05-07 14:14:24 +0100

    vb

--- src/scripts/neuekit.js
+++ src/scripts/neuekit.js
@@ -1,7 +1,7 @@
 /*!---------------------------------------------------------------------------*\
     Neue Kit
 
-    - Version:      1.0.0.alpha.11
+    - Version:      1.0.0.alpha.12
     - Author:       Creative Little Dots
     - Site:         http://creativelittle.uk/neuekit
     - Copyright:    2019, Creative Little Dots
