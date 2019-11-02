6f44557cd60d257df39d5273aa23da948d1b274b
Matt Pilott <matt@creativelittledots.co.uk>
2019-11-02 15:04:57 +0000

    node-sass => sass, and deps + no more shame

--- src/scripts/neuekit.js
+++ src/scripts/neuekit.js
@@ -1,7 +1,7 @@
 /*!---------------------------------------------------------------------------*\
     Neue Kit
 
-    - Version:      1.0.0.alpha.12
+    - Version:      1.0.0.alpha.15
     - Author:       Creative Little Dots
     - Site:         http://creativelittle.uk/neuekit
     - Copyright:    2019, Creative Little Dots
